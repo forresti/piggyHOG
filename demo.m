3d5ae9983460d7f0ff4bc9dc2c448864f0aa7b95
forresti <fiandola@gmail.com>
2013-08-27 17:11:15 -0700

    DPM car model

--- demo.m
+++ demo.m
@@ -1,6 +1,15 @@
 
 function demo()
-    
+    img_dir = 'ig02-v1.0-cars/cars';
+    imgs = dir([car_dir '/*.png']) %640x480 car images
+    %output_dir = './cascade-results-g02-v1.0-cars';
+    load('VOC2007/car_final');
+
+    for idx = 1:length(car_imgs)
+        car_img = car_imgs(idx).name
+        test([car_dir '/' car_img], model, useCascade);
+        print(gcf, '-dpng', '-r0', [output_dir '/' car_img]);
+    end
 
 
 function pyra = time_extract_hog(im)
