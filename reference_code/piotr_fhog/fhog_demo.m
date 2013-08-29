70fd1ecdde3776eb7344ac07d0b96b4929172282
forresti <fiandola@gmail.com>
2013-08-28 18:35:03 -0700

    refined demo of Piotr's hog

--- reference_code/piotr_fhog/fhog_demo.m
+++ reference_code/piotr_fhog/fhog_demo.m
@@ -1,6 +1,7 @@
 
-%im = imread('~/vision_experiments/voc-release5/ig02-v1.0-cars/cars/carsgraz_420.image.png');
 im = imread('../../images_640x480/carsgraz_001.image.png');
 I=imResample(single(im)/255,[480 640]);
-tic, for i=1:100, H=fhog(I,8,9); end; disp(100/toc)
+tic 
+for i=1:100, H=fhog(I,8,9); end; 
+disp([num2str(100/toc) ' fps'])
 
