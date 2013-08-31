421058af47bd07f4069518cbc6b70cc02c3e8929
forresti <fiandola@gmail.com>
2013-08-30 17:43:10 -0700

    forking off a version of demo.m to serve as the voc5 test that produces a CSV file

---
+++ tests/voc5_features_test.m
@@ -0,0 +1,37 @@
+
+function demo()
+    img_dir = 'images_640x480';
+    imgs = dir([img_dir '/*.png']); %640x480 car images
+    %output_dir = './cascade-results-g02-v1.0-cars';
+    load('VOC2007/car_final'); %load 'model' structure
+
+    for idx = 1:length(imgs)
+        curr_img = imgs(idx).name
+        pyra = time_extract_hog(curr_img, model);
+
+        visHog(pyra)
+        %print(gcf, '-dpng', '-r0', [output_dir '/' curr_img]);
+    end
+
+function pyra = time_extract_hog(img_name, model)
+    im = imread(img_name);
+    %im = color(im);
+
+    th = tic();
+    pyra = featpyramid(double(im), model);
+    %pyra = featpyramid_fhog(single(im), model);
+    tF = toc(th);
+    fprintf('  --> HOG pyramid extraction took %f seconds\n', tF);
+
+function visHog(pyra)
+    %just visualize top HOG level
+    
+    nlevels = length(pyra.feat);
+
+    for level = 1:10:nlevels
+        figure(level)
+        w = foldHOG(pyra.feat{level});
+        visualizeHOG(double(max(0,w)));
+    end    
+
+
