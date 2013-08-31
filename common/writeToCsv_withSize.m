ceae127b84ed4fd49475a5a7ae417f6e9c92935a
forresti <fiandola@gmail.com>
2013-08-30 18:00:51 -0700

    common functions for tests, e.g. writeCsv_withSize

---
+++ common/writeToCsv_withSize.m
@@ -0,0 +1,53 @@
+
+
+function writeToCsv_withSize(fname, array)
+    csvwrite(fname, array);
+
+    mySize = size(array);
+    if(length(size(array)) == 2)
+        sizeStr = sprintf('%d,%d', mySize(1), mySize(2));
+    elseif (length(size(array)) == 3)
+        sizeStr = sprintf('%d,%d,%d', mySize(1), mySize(2), mySize(3));
+    end
+
+    prepend2file(sizeStr, fname, true);
+    %prepend2file([num2str], fname, true)
+end
+
+%thanks to: http://www.mathworks.com/support/solutions/en/data/1-1BM4K/index.html?product=SL&solution=1-1BM4K
+function prepend2file( string, filename, newline )
+    % newline: is an optional boolean, that if true will append a \n to the end
+    % of the string that is sent in such that the original text starts on the
+    % next line rather than the end of the line that the string is on
+    % string: a single line string
+    % filename: the file you want to prepend to
+    tempFile = tempname;
+    fw = fopen( tempFile, 'wt' );
+    if nargin < 3
+    newline = false;
+    end
+    if newline
+    fwrite( fw, sprintf('%s\n', string ) );
+    else
+    fwrite( fw, string );
+    end
+
+    fclose( fw );
+    appendFiles( filename, tempFile );
+    copyfile( tempFile, filename );
+    delete(tempFile);
+
+    % append readFile to writtenFile
+    function status = appendFiles( readFile, writtenFile )
+    fr = fopen( readFile, 'rt' );
+    fw = fopen( writtenFile, 'at' );
+
+    while feof( fr ) == 0
+    tline = fgetl( fr );
+    fwrite( fw, sprintf('%s\n',tline ) );
+    end
+    fclose(fr);
+    fclose(fw);
+    end
+end
+
