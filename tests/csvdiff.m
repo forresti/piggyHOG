534b5cb670e6305c7f99f7562a6b6be32ea463a6
forresti <fiandola@gmail.com>
2013-09-01 15:24:26 -0700

    FFLD and VOC5 match reasonably well. with a diff threshold of 0.1, there are ~5% differences. This is true for all BUT the top level. At the top level (hog(1) in voc5, hog(0) in FFLD), there are a lot of differences. not sure what's causing this. need to dig deeper.

--- tests/csvdiff.m
+++ tests/csvdiff.m
@@ -12,12 +12,10 @@ function mydiff(ffldCsv, voc5Csv, h)
             voc5Result = csvread(voc5Csv);
             voc5Result = voc5Result(2:end, :); %remove header (which shows dims)
 
-keyboard
-
-            thresh = 0.0001;
+            thresh = 0.1;
             diff = abs(ffldResult - voc5Result);
             resultSize = size(ffldResult);
-            display(['hog[' int2str(h) ']*filter[' int2str(f) ']:'])
+            display(['hog[' int2str(h) ']:'])
             display(['    size(result) = ' mat2str(size(ffldResult)) ' = ' num2str(resultSize(1)*resultSize(2))])
             display(['    nnz(diff) = ' num2str(nnz(diff))])
             display(['    num diff elements above ' num2str(thresh) ' = ' num2str(nnz(diff>=thresh)) ])
