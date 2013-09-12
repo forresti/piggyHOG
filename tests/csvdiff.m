91eb1adb7df2b7eb380a32e93e3b4779fc53b4c6
forresti <fiandola@gmail.com>
2013-09-12 12:32:38 -0700

    faster csvwrite done ... and results match my orig 'repeated dlmwrite' impl. (should probably do some more testing; will go on R8 and diff with FFLD when I land)

--- tests/csvdiff.m
+++ tests/csvdiff.m
@@ -2,8 +2,8 @@ function csvdiff()
 
     %for h=1:10:41
     for h=1:41
-        %experimentalCsv = ['./ffld_results/level' int2str(h-1) '.csv']; %h-1 for C++ 0-indexing
-        experimentalCsv = ['./piotr_results/carsgraz_001.image_scale_' int2str(h) '.csv'];
+        experimentalCsv = ['./ffld_results/level' int2str(h-1) '.csv']; %h-1 for C++ 0-indexing
+        %experimentalCsv = ['./piotr_results/carsgraz_001.image_scale_' int2str(h) '.csv'];
         referenceCsv = ['./voc5_features_results/carsgraz_001.image_scale_' int2str(h) '.csv']; %VOC5 is our baseline reference 
         mydiff(experimentalCsv, referenceCsv, h)
     end
@@ -11,7 +11,7 @@ end
 
 function mydiff(experimentalCsv, referenceCsv, h)
     experimentalResult = csvread(experimentalCsv);
-    experimentalResult = experimentalResult(2:end, :); %remove header (which shows dims)
+    %experimentalResult = experimentalResult(2:end, :); %remove header (which shows dims)
     referenceResult = csvread(referenceCsv);
     referenceResult = referenceResult(2:end, :); %remove header (which shows dims)
 
@@ -22,10 +22,10 @@ function mydiff(experimentalCsv, referenceCsv, h)
 
     resultSize = inHeight * inWidth;
     display(['hog[' int2str(h) ']:'])
-    display(['    size(referenceResult) = ' mat2str(size(referenceResult)) ' = ' num2str(inHeight_voc5*inWidth_voc5)])
-    display(['    size(experimentalResult) = ' mat2str(size(experimentalResult)) ' = ' num2str(resultSize)])
+    %display(['    size(referenceResult) = ' mat2str(size(referenceResult)) ' = ' num2str(inHeight_voc5*inWidth_voc5)])
+    %display(['    size(experimentalResult) = ' mat2str(size(experimentalResult)) ' = ' num2str(resultSize)])
     %display(['    nnz(diff) = ' num2str(nnz(diff))])
     %display(['    num diff elements above ' num2str(thresh) ' = ' num2str(nnz(diff>=thresh)) ])
-    %display(['    percent mismatches above ' num2str(thresh) ' = ' num2str(nnz(diff>=thresh)/resultSize * 100) '%'])
+    display(['    percent mismatches above ' num2str(thresh) ' = ' num2str(nnz(diff>=thresh)/resultSize * 100) '%'])
 end
 
