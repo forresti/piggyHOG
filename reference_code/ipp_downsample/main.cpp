54de22d1af4f69f07378294bf3fd19def3d8a617
forresti <fiandola@gmail.com>
2013-09-16 01:06:21 -0700

    trying to repro my old conv example using IPP, but 3 channels. hmph. getting just arbitrary static

--- reference_code/ipp_downsample/main.cpp
+++ reference_code/ipp_downsample/main.cpp
@@ -11,17 +11,23 @@ using namespace std;
 using namespace cv;
 
 //returns execution time of ippiFilter
-double runIppConvolution(int imgRows, int imgCols, int kernelRows, int kernelCols, string dataType)
+//double runIppConvolution(int imgRows, int imgCols, int kernelRows, int kernelCols, string dataType)
+double demoIppConvolution(int kernelRows, int kernelCols) //just use Lena.png
 {
+    Mat img = imread("./Lena.pgm");
+#if 0
     Mat img;
     if(dataType == "8u")
         img = Mat::zeros(imgRows, imgCols, CV_8UC1);
     if(dataType == "32f")
         img = Mat::zeros(imgRows, imgCols, CV_32FC1);
-
+#endif
     Mat outImg = img.clone(); //allocate space for the convolution results 
-    
-    int step = imgCols; //pitch
+
+    int imgCols = img.cols;
+    int imgRows = img.rows;
+
+    int step = imgRows*3; //pitch
     const Ipp32s kernel_int[kernelRows*kernelCols]; //zeros
     const Ipp32f kernel_float[kernelRows*kernelCols];
     IppiSize kernelSize = {kernelCols, kernelRows};
@@ -31,20 +37,25 @@ double runIppConvolution(int imgRows, int imgCols, int kernelRows, int kernelCol
     IppStatus status;
 
     double start = read_timer();
-    if(dataType == "8u"){
-        status = ippiFilter_8u_C1R((const Ipp8u*)&img.data[0], step, 
+    //if(dataType == "8u"){
+        status = ippiFilter_8u_C3R((const Ipp8u*)&img.data[0], step, 
                                    (Ipp8u*)&outImg.data[0], step, dstRoiSize, 
                                    kernel_int, kernelSize, anchor, divisor);
-    }
+    //}
+#if 0
     if(dataType == "32f"){
         step*=4; //pitch is in bytes
         status = ippiFilter_32f_C1R((const Ipp32f*)&img.data[0], step,
                                    (Ipp32f*)&outImg.data[0], step, dstRoiSize,          
                                    kernel_float, kernelSize, anchor);
     }
+#endif
     double execTime = read_timer() - start;
     if(status != 0) 
         cout << "IppiFilter error status " << ippGetStatusString(status) << endl;
+
+    forrestWritePgm(outImg, "Lena_ipp.pgm");    
+
     return execTime;
 }
 
@@ -52,22 +63,23 @@ void benchmarkIppConvolution()
 {
     int imgRows = 9000;
     int imgCols = 9000;
-    string dataType = "8u";
+    string dataType = "32f";
     //for(int kernelSize=2; kernelSize<128; kernelSize*=2)
     int kernelSize=5;
     for(int i=0; i<10; i++)
     {
-        double execTime = runIppConvolution(imgRows, imgCols, kernelSize, kernelSize, dataType);
+        //double execTime = runIppConvolution(imgRows, imgCols, kernelSize, kernelSize, dataType);
+        double execTime = demoIppConvolution(kernelSize, kernelSize);
         printf("imgSize = %dx%d,  kernelSize = %d,  execTime = %f ms \n", imgCols, imgRows, kernelSize, execTime);
     }
-
+#if 0
     kernelSize = 4;
     for(int imgSize=256; imgSize<5000; imgSize*=2) //imgSize = imgRows = imgCols
     {
         double execTime = runIppConvolution(imgSize, imgSize, kernelSize, kernelSize, dataType);
         printf("imgSize = %dx%d,  kernelSize = %d,  execTime = %f ms \n", imgSize, imgSize, kernelSize, execTime);
     }
-
+#endif
 }
 
 int main (int argc, char **argv)
