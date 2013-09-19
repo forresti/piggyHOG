928dfadbfca6b8851fefa985aa0b2ce066051ccd
forresti <fiandola@gmail.com>
2013-09-19 10:10:34 -0700

    working on compiling with piotr's code

--- reference_code/piotr_fhog_cpp/gradientMex.h
+++ reference_code/piotr_fhog_cpp/gradientMex.h
@@ -1,6 +1,10 @@
 #ifndef __GRADIENT_MEX_H__
 #define __GRADIENT_MEX_H__
 
+void gradMag(float *I, float *M, float *O, int h, int w, int d, bool full);
+
+void fhog(float *M, float *O, float *H, int h, int w, int binSize,
+          int nOrients, int softBin, float clip);
 
 
 #endif
