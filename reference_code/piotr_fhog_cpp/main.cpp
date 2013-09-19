16a0193004187aac94fbbc665d49b15b71a4d3a0
forresti <fiandola@gmail.com>
2013-09-19 10:03:34 -0700

    going to set up cpp frontend for piotr's fhog code

---
+++ reference_code/piotr_fhog_cpp/main.cpp
@@ -0,0 +1,22 @@
+
+//#include <cuda.h>
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include "helpers.h"
+using namespace std;
+
+int main (int argc, char **argv)
+{
+    cv::Mat x;
+    printf("%f \n", foo(1.2345));
+// What will this print
+float f = 1.0;
+int  *p = (int*)(&f);
+
+printf("%d\n", *p);
+
+    return 0;
+}
