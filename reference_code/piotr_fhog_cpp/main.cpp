c12f974ba834e3d8c10e8b04fefbe5bf3b282b24
forresti <fiandola@gmail.com>
2013-09-19 17:57:31 -0700

    loop reordering (will matter for efficiency at some point). Still same problem with img_data[y*w*d] segfaulting

--- reference_code/piotr_fhog_cpp/main.cpp
+++ reference_code/piotr_fhog_cpp/main.cpp
@@ -21,11 +21,11 @@ float* transpose_opencv_to_matlab(Mat img){
     uchar* img_data = &img_data[0];
     float* I = (float*)malloc(h * w * d * sizeof(float)); //img transposed to Matlab data layout. (TODO: pad for SSE?)
 
-    for(int x=0; x<w; x++){
-        for(int y=0; y<h; y++){
+    for(int y=0; y<h; y++){
+        for(int x=0; x<w; x++){
             for(int ch=0; ch<d; ch++){
                 //I[x*h + y + ch*w*h] = img_data[x*d + y*w*d + ch] * multiplier; //TODO: cast img_data to float?
-                I[x*h + y   + ch*w*h] = img_data[x*d + y*w + ch]; //temp for debug -- why does y*w*d cause a segfault?
+                I[x*h + y   + ch*w*h] = img_data[y*w*d]; //FIXME: temp for debug -- why does y*w*d cause a segfault?
             }
         }
     }
