cdb0638b99045e38e965647f64422dd8e1d0fd0d
forresti <fiandola@gmail.com>
2013-09-19 18:05:15 -0700

    aha! I somehow did img_data = &img_data[0]; instead of img_data = &img.data[0];. Not sure why this would even compile. Segfault is gone now.

--- reference_code/piotr_fhog_cpp/main.cpp
+++ reference_code/piotr_fhog_cpp/main.cpp
@@ -18,15 +18,15 @@ float* transpose_opencv_to_matlab(Mat img){
     int d = 3; //nChannels
     float multiplier = 1 / 255.0f; //rescale pixels to range [0 to 1]
 
-    uchar* img_data = &img_data[0];
+    uchar* img_data = &img.data[0];
     float* I = (float*)malloc(h * w * d * sizeof(float)); //img transposed to Matlab data layout. (TODO: pad for SSE?)
 
     for(int y=0; y<h; y++){
         for(int x=0; x<w; x++){
             for(int ch=0; ch<d; ch++){
-                //I[x*h + y + ch*w*h] = img_data[x*d + y*w*d + ch] * multiplier; //TODO: cast img_data to float?
+                I[x*h + y + ch*w*h] = img_data[x*d + y*w*d + ch] * multiplier; //TODO: cast img_data to float?
                 //I[x*h + y   + ch*w*h] = img_data[y*w*d]; //FIXME: temp for debug -- why does y*w*d cause a segfault?
-                img_data[y*w*d] = (uchar)0;
+                //img.data[y*w*d] = (uchar)0; //TEST -- segfault
             }
         }
     }
