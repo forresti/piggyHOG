0eecd0d25807d05dcdfc822328876f581b718f63
forresti <fiandola@gmail.com>
2013-09-20 14:19:48 -0700

    diffing C++ and Matlab frontend for piotr's code

--- tests/csvdiff_piotr_cpp.m
+++ tests/csvdiff_piotr_cpp.m
@@ -1,9 +1,11 @@
 function csvdiff()
     %for h=1:10:41
-    for h=1:41
+    %for h=1:41
+    for h=11:11
         %experimentalCsv = ['./ffld_results/level' int2str(h-1) '.csv']; %h-1 for C++ 0-indexing
-        experimentalCsv = ['./piotr_results/carsgraz_001.image_scale_' int2str(h) '.csv'];
-        referenceCsv = ['./voc5_features_results/carsgraz_001.image_scale_' int2str(h) '.csv']; %VOC5 is our baseline reference 
+
+        experimentalCsv = ['piotr_fhog_cpp_results/level0.csv']; %just doing 1 level at the moment ... corresponds to 'level 11' in pyramid 
+        referenceCsv = ['./piotr_results/carsgraz_001.image_scale_' int2str(h) '.csv'];
         mydiff(experimentalCsv, referenceCsv, h)
     end
 end
@@ -13,10 +15,10 @@ function mydiff(experimentalCsv, referenceCsv, h)
     referenceResult = referenceResult(2:end, :); %remove header (which shows dims)
 
     experimentalResult = csvread(experimentalCsv);
-    experimentalResult = experimentalResult(2:end, :); %remove header (which shows dims)
+    %experimentalResult = experimentalResult(2:end, :); %remove header (which shows dims)
 
     thresh = 0.1;
-    diff = abs(experimentalResult - referenceResult);
+    diff = abs(experimentalResult - referenceResult)
     [inHeight_voc5 inWidth_voc5] = size(referenceResult);
     [inHeight inWidth] = size(experimentalResult);
 
@@ -28,14 +30,8 @@ function mydiff(experimentalCsv, referenceCsv, h)
     %display(['    size(referenceResult) = ' mat2str(size(referenceResult)) ' = ' num2str(inHeight_voc5*inWidth_voc5)])
     %display(['    size(experimentalResult) = ' mat2str(size(experimentalResult)) ' = ' num2str(resultSize)])
 
-    %display(['    nnz(diff) = ' num2str(nnz(diff))])
-    %display(['    num diff elements above ' num2str(thresh) ' = ' num2str(nnz(diff>=thresh)) ])
+    display(['    nnz(diff) = ' num2str(nnz(diff))])
     display(['    percent mismatches above ' num2str(thresh) ' = ' num2str(nnz(diff>=thresh)/resultSize * 100) '%'])
 end
 
-%if one of the HOG results is bigger than the other, trim it down.
-% naive: just taking the top-left corner when trimming.
-%[referenceResult experimentalResult] = trimToMinSize(referenceResult, experimentalResult)
-    %[h w d] = reshape.... %ah, crap, need the original dims.
-    %minWidth = 
 
