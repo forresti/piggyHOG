1bc374c5a7ea4c90cc380eb1cd0bc5df47c84f80
forresti <fiandola@gmail.com>
2013-09-22 20:00:43 -0700

    nevermind ... just needed to make clean. does build on mac.

--- reference_code/piotr_fhog_cpp/main.cpp
+++ reference_code/piotr_fhog_cpp/main.cpp
@@ -101,7 +101,7 @@ Mat piotr_fhog_wrapper_1img(Mat img){
     float* H = (float*)calloc(hogHeight * hogWidth * hogDepth, sizeof(float)); //TODO: are these dims correct? Should do a "hogH, hogW, hogD?" 
 
 //BEGIN DEBUG
-    //Mat magnitudes(h, w, CV_32FC1, M);
+    Mat magnitudes(h, w, CV_32FC1, M);
 
 //END DEBUG
 
