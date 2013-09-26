9b47f13988e3bdc6ce256288f85309d17fe55800
forresti <fiandola@gmail.com>
2013-09-26 12:16:04 -0700

    fixed paths

--- piggyHOG_src/build/demo.sh
+++ piggyHOG_src/build/demo.sh
@@ -1,3 +1,3 @@
-#./ffld ../../../images_640x480/carsgraz_001.image.jpg
-./ffld --padding 8 ../../../images_640x480/carsgraz_001.image.jpg
+#./ffld ../../images_640x480/carsgraz_001.image.jpg
+./ffld --padding 8 ../../images_640x480/carsgraz_001.image.jpg
 
