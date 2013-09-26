37225b05403b7547e269fef2336fade42076aee7
forresti <fiandola@gmail.com>
2013-09-26 16:58:31 -0700

    fleshing out internal APIs

--- piggyHOG_src/PgHogContainer.h
+++ piggyHOG_src/PgHogContainer.h
@@ -2,13 +2,12 @@
 #define __PgHogContainer_H__
 #include <opencv2/opencv.hpp>
 
-
+//one HOG level
 typedef struct PgHogContainer{
     float* hog;
     int width;
     int height;
     int depth; //typically 32
-
 }PgHogContainer;
 
 #endif
