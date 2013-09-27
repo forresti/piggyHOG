78a38865e3ce15956fded8497905a41fb7760224
forresti <fiandola@gmail.com>
2013-09-26 17:06:25 -0700

    setting up hogResult

--- piggyHOG_src/PgHog.cpp
+++ piggyHOG_src/PgHog.cpp
@@ -12,7 +12,20 @@ PgHog::~PgHog(){
 
 //compute the gradient and magnitude at one image location, store the results in gradImg and magImg
 void PgHog::gradient(int x, int y, Mat &gradImg, Mat &magImg){
-
+    
 
 }
+
+
+PgHogContainer extract_HOG_oneScale(Mat img, int spatialBinSize){
+    PgHogContainer hogResult;
+
+    hogResult.width = round((float)img.cols / (float)spatialBinSize);
+    hogResult.height = round((float)img.rows / (float)spatialBinSize);
+    hogResult.depth = 32;
+
+    hogResult.hog = (float*)malloc(hogResult.width * hogResult.height * hogResult.depth);
+
+}
+
 
