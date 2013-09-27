c64ce49dc06603003acb6d510a6825ef5ee3c10b
forresti <fiandola@gmail.com>
2013-09-26 17:21:21 -0700

    gradient boilerplate

--- piggyHOG_src/PgHog.cpp
+++ piggyHOG_src/PgHog.cpp
@@ -17,20 +17,35 @@ void PgHog::gradient(int x, int y, Mat &gradImg, Mat &magImg){
 }
 
 
-PgHogContainer extract_HOG_oneScale(Mat img, int spatialBinSize){
+PgHogContainer PgHog::extract_HOG_oneScale(Mat img, int spatialBinSize){
+  //setup
     assert(img.type() == CV_8UC3);
 
-    Mat gradImg(img.rows, img.cols, CV_8UC1);
+    Mat gradImg(img.rows, img.cols, CV_8UC1); //TODO: replace with aligned mem?
     Mat magImg(img.rows, img.cols, CV_8UC1);
 
+    //hogResult first holds HOG Cells, then is normalized into HOG Blocks.
     PgHogContainer hogResult;
     hogResult.width = round((float)img.cols / (float)spatialBinSize);
     hogResult.height = round((float)img.rows / (float)spatialBinSize);
     hogResult.depth = 32;
     hogResult.hog = (float*)malloc(hogResult.width * hogResult.height * hogResult.depth);
 
-    
+    //TODO: store normalization results
+    //float* norm = malloc(hogResult.width * hogResult.height); 
 
+  //extract features
+    for(int hogY = 0; hogY < hogResult.height; hogY++){
+        for(int hogX = 0; hogX < hogResult.width; hogX++){
+
+            for(int y=0; y<spatialBinSize; y++){ //TODO: move these loops into PgHog::gradient()?
+                for(int x=0; x<spatialBinSize; x++){
+                    PgHog::gradient(hogX + x, hogY + y, gradImg, magImg); 
+                }
+            }
+
+        }
+    }
 }
 
 
