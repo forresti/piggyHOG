c77c9bad1bc2d211f5445f78a12689aa68aaa9d1
forresti <fiandola@gmail.com>
2013-09-26 17:57:59 -0700

    working on gradient

--- piggyHOG_src/PgHog.cpp
+++ piggyHOG_src/PgHog.cpp
@@ -11,9 +11,17 @@ PgHog::~PgHog(){
 }
 
 //compute the gradient and magnitude at one image location, store the results in gradImg and magImg
-void PgHog::gradient(int x, int y, Mat &gradImg, Mat &magImg){
-    
+void PgHog::gradient(int x, int y, Mat img, Mat &gradImg, Mat &magImg){
 
+    //these aren't clamped; be careful!
+    // make sure x is in range (1, width-1), and y is in range (1, height-1) 
+
+    float gradX;
+    float gradY;
+
+    for(int channel=0; channel<3; channel++){
+        float gradX_tmp = img.at<cv::Vec3b>(y,x+1)[channel] - img.at<cv::Vec3b>(y,x-1)[channel];
+    }
 }
 
 
@@ -35,13 +43,15 @@ PgHogContainer PgHog::extract_HOG_oneScale(Mat img, int spatialBinSize){
     //float* norm = malloc(hogResult.width * hogResult.height); 
 
   //extract features
-    for(int hogY = 0; hogY < hogResult.height; hogY++){
-        for(int hogX = 0; hogX < hogResult.width; hogX++){
-
+    //for(int hogY = 0; hogY < hogResult.height; hogY++){
+    //    for(int hogX = 0; hogX < hogResult.width; hogX++){
+    for(int hogY = 1; hogY < hogResult.height-1; hogY++){ //start from 1 to avoid needing to clamp
+        for(int hogX = 1; hogX < hogResult.width-1; hogX++){
+ 
             for(int y=0; y<spatialBinSize; y++){ //TODO: move these loops into PgHog::gradient()?
                 for(int x=0; x<spatialBinSize; x++){
                     //update gradImg and magImg at this x,y location
-                    PgHog::gradient(hogX + x, hogY + y, gradImg, magImg);  
+                    PgHog::gradient(hogX + x, hogY + y, img, gradImg, magImg);  
                 }
             }
 
