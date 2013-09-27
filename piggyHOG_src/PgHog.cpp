56a54488c9e0acdfed0e7d8743d5c6c3337ea36e
forresti <fiandola@gmail.com>
2013-09-26 17:09:26 -0700

    intermediate results

--- piggyHOG_src/PgHog.cpp
+++ piggyHOG_src/PgHog.cpp
@@ -18,13 +18,18 @@ void PgHog::gradient(int x, int y, Mat &gradImg, Mat &magImg){
 
 
 PgHogContainer extract_HOG_oneScale(Mat img, int spatialBinSize){
-    PgHogContainer hogResult;
+    assert(img.type() == CV_8UC3);
 
+    Mat gradImg(img.rows, img.cols, CV_8UC1);
+    Mat magImg(img.rows, img.cols, CV_8UC1);
+
+    PgHogContainer hogResult;
     hogResult.width = round((float)img.cols / (float)spatialBinSize);
     hogResult.height = round((float)img.rows / (float)spatialBinSize);
     hogResult.depth = 32;
-
     hogResult.hog = (float*)malloc(hogResult.width * hogResult.height * hogResult.depth);
+
+    
 
 }
 
