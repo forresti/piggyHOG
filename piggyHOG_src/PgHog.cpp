81bee7e690d6418c4e8c9b2c14a1cc6f199fdca4
forresti <fiandola@gmail.com>
2013-09-26 19:03:02 -0700

    rename: gradImg -> oriImg

--- piggyHOG_src/PgHog.cpp
+++ piggyHOG_src/PgHog.cpp
@@ -10,18 +10,33 @@ PgHog::~PgHog(){
 
 }
 
-//compute the gradient and magnitude at one image location, store the results in gradImg and magImg
-void PgHog::gradient(int x, int y, Mat img, Mat &gradImg, Mat &magImg){
+//compute the gradient and magnitude at one image location, store the results in oriImg and magImg
+void PgHog::gradient(int x, int y, Mat img, Mat &oriImg, Mat &magImg){
 
-    //these aren't clamped; be careful!
+    //accesses to 'img' aren't clamped; be careful!
     // make sure x is in range (1, width-1), and y is in range (1, height-1) 
 
     float gradX;
     float gradY;
+    float max_mag = 0.0f; //TODO: check range ... can this be uchar?
 
     for(int channel=0; channel<3; channel++){
-        float gradX_tmp = img.at<cv::Vec3b>(y,x+1)[channel] - img.at<cv::Vec3b>(y,x-1)[channel];
+        //TODO: index the data directly instead of using .at
+        float tmp_gradX = img.at<cv::Vec3b>(y,x+1)[channel] - img.at<cv::Vec3b>(y,x-1)[channel];
+        float tmp_gradY = img.at<cv::Vec3b>(y+1,x)[channel] - img.at<cv::Vec3b>(y-1,x)[channel];
+        float tmp_mag = tmp_gradX*tmp_gradX + tmp_gradY*tmp_gradY;       
+
+        if(tmp_mag > max_mag){
+            gradX = tmp_gradX;
+            gradY = tmp_gradY;
+            max_mag = tmp_mag;
+        } 
     }
+    //this is the gradient angle
+    float grad = atan2((double)gradY, (double)gradX); //does float vs. double matter here? 
+
+    oriImg.at<float>(y, x) = grad;
+    magImg.at<float>(y, x)  = max_mag;
 }
 
 
@@ -29,8 +44,9 @@ PgHogContainer PgHog::extract_HOG_oneScale(Mat img, int spatialBinSize){
   //setup
     assert(img.type() == CV_8UC3);
 
-    Mat gradImg(img.rows, img.cols, CV_8UC1); //TODO: replace with aligned mem?
-    Mat magImg(img.rows, img.cols, CV_8UC1);
+    //TODO: possibly go down to 8-bit char
+    Mat oriImg(img.rows, img.cols, CV_32FC1); //TODO: replace with aligned mem?
+    Mat magImg(img.rows, img.cols, CV_32FC1);
 
     //hogResult first holds HOG Cells, then is normalized into HOG Blocks.
     PgHogContainer hogResult;
@@ -50,8 +66,8 @@ PgHogContainer PgHog::extract_HOG_oneScale(Mat img, int spatialBinSize){
  
             for(int y=0; y<spatialBinSize; y++){ //TODO: move these loops into PgHog::gradient()?
                 for(int x=0; x<spatialBinSize; x++){
-                    //update gradImg and magImg at this x,y location
-                    PgHog::gradient(hogX + x, hogY + y, img, gradImg, magImg);  
+                    //update oriImg and magImg at this x,y location
+                    PgHog::gradient(hogX + x, hogY + y, img, oriImg, magImg);  
                 }
             }
 
