78d55e9656440999fd557449e7fbc005b0d3a5a9
forresti <fiandola@gmail.com>
2013-09-29 14:25:56 -0700

    yeah, 48 pixels contribute to hog cell (y=6, x=16) when we're doing just 1 level with sbin=4.

--- reference_code/ffld/HOGPyramid.cpp
+++ reference_code/ffld/HOGPyramid.cpp
@@ -295,8 +295,8 @@ template <class Matrix, int CellSize>
 	const int c = l * 2 + 1;
 	const int d = CellSize * 2 - c;
 
-    //if(CellSize == 4)
-        //printf("pixel (y=%d,x=%d) goes to HOG cells (j,i) = (%d,%d), (%d,%d), (%d,%d), (%d,%d) \n", y,x, i,j, i+1,j, i,j+1, i+1,j+1);
+    if(CellSize == 4)
+        printf("pixel (y=%d,x=%d) goes to HOG cells (j,i) = (%d,%d), (%d,%d), (%d,%d), (%d,%d) \n", y,x, i,j, i+1,j, i,j+1, i+1,j+1);
 	
 	matrix(i    , j    )(bin0) += magnitude0 * (b * d);
 	matrix(i    , j    )(bin1) += magnitude1 * (b * d);
