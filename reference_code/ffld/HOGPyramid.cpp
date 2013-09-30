25bb9092bbb2e47bdac68cf0cdab61e54e294d44
forresti <fiandola@gmail.com>
2013-09-29 17:32:05 -0700

    looks like weightX and weightY in FFLD are in the range 1 to 8.

--- reference_code/ffld/HOGPyramid.cpp
+++ reference_code/ffld/HOGPyramid.cpp
@@ -299,8 +299,10 @@ template <class Matrix, int CellSize>
 
     //printf("pixel (y=%d,x=%d) goes to HOG cells (j,i) = (%d,%d), (%d,%d), (%d,%d), (%d,%d) \n", y,x, i,j, i+1,j, i,j+1, i+1,j+1);
 
-    printf("weights: %d, %d, %d, %d \n", b*d, b*c, a*d, a*c);
-	
+    //printf("weights: %d, %d, %d, %d \n", b*d, b*c, a*d, a*c);
+    printf("weightX = %d, weightY = %d \n", d, b);	
+
+
 	matrix(i    , j    )(bin0) += magnitude0 * (b * d);
 	matrix(i    , j    )(bin1) += magnitude1 * (b * d);
 	matrix(i    , j + 1)(bin0) += magnitude0 * (b * c);
