3e78066dd6bd7fa84d6d05733bd47eea7c6f2014
forresti <fiandola@gmail.com>
2013-10-05 20:04:26 -0700

    aha, the clamp() code that I copy/pasted off the internet had an off-by-one. fixed.

---
+++ reference_code/halide_hog/gradient.cpp
@@ -0,0 +1,34 @@
+#include <Halide.h>
+using namespace Halide;
+
+int main(int argc, char **argv) {
+
+    ImageParam input(UInt(8), 3);
+//    ImageParam output(Float(32), 2);
+    Func grad_x("grad_x"), grad_y("grad_y");
+    Var x("x"), y("y"), ch("ch");
+    Var xi("xi"), yi("yi");
+    Func clamped("clamped");
+
+    clamped(x,y,ch) = input(clamp(x,0,input.width()-1), clamp(y,0,input.height()-1), ch);
+    
+#if 0
+    // The algorithm
+    grad_x(x, y) = (input(x, y) + input(x+1, y) + input(x+2, y))/3;
+    grad_y(x, y) = (grad_x(x, y) + grad_x(x, y+1) + grad_x(x, y+2))/3;
+    
+    // How to schedule it
+    grad_y.split(y, y, yi, 8).parallel(y).vectorize(x, 8);
+    grad_x.store_at(grad_y, y).compute_at(grad_y, yi).vectorize(x, 8);  
+    
+    grad_y.compile_to_file("gradient", input); 
+#endif
+
+    grad_x(x, y, ch) = clamped(x+1, y, ch) - clamped(x-1, y, ch);
+    //grad_x.compile_to_file("gradient", input, output); 
+    grad_x.compile_to_file("gradient", input);
+        
+//TODO: how I set things up so that 'output' is a separate space from 'input'?
+
+    return 0;
+}
