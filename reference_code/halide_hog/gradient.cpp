94afad02a8b6c24b23c9044e70d60f7f7deb7f97
forresti <fiandola@gmail.com>
2013-10-05 23:24:53 -0700

    set up gradY. cleanup commented-out code.

--- reference_code/halide_hog/gradient.cpp
+++ reference_code/halide_hog/gradient.cpp
@@ -5,7 +5,7 @@ int main(int argc, char **argv) {
 
     ImageParam input(UInt(8), 3);
 //    ImageParam output(Float(32), 2);
-    Func grad_x("grad_x"), grad_y("grad_y");
+    Func gradX_rgb("gradX_rgb"), gradY_rgb("gradY_rgb");
     Var x("x"), y("y"), ch("ch");
     Var xi("xi"), yi("yi");
 
@@ -15,21 +15,11 @@ int main(int argc, char **argv) {
     //thanks for cast idea: github.com/halide/Halide/blob/master/tutorial/lesson_02.cpp    
     //Expr input_as_float = Halide::cast<float>(input);
 
-#if 0
-    // The algorithm
-    grad_x(x, y) = (input(x, y) + input(x+1, y) + input(x+2, y))/3;
-    grad_y(x, y) = (grad_x(x, y) + grad_x(x, y+1) + grad_x(x, y+2))/3;
-    
-    // How to schedule it
-    grad_y.split(y, y, yi, 8).parallel(y).vectorize(x, 8);
-    grad_x.store_at(grad_y, y).compute_at(grad_y, yi).vectorize(x, 8);  
-    
-    grad_y.compile_to_file("gradient", input); 
-#endif
+    gradX_rgb(x, y, ch) = cast<uint8_t>(cast<float>(clamped(x+1, y, ch)) - cast<float>(clamped(x-1, y, ch))); //uint8_t cast is temporary
+    gradY_rgb(x, y, ch) = cast<float>(clamped(x, y+1, ch)) - cast<float>(clamped(x, y-1, ch));
 
-    grad_x(x, y, ch) = cast<uint8_t>(cast<float>(clamped(x+1, y, ch)) - cast<float>(clamped(x-1, y, ch)));
-    //grad_x.compile_to_file("gradient", input, output); 
-    grad_x.compile_to_file("gradient", input);
+    //gradX_rgb.compile_to_file("gradient", input, output); 
+    gradX_rgb.compile_to_file("gradient", input);
         
 //TODO: how I set things up so that 'output' is a separate space from 'input'?
 
