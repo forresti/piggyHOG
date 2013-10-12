1529d95a444fa67044e3316589eba4345bb8b727
forresti <fiandola@gmail.com>
2013-10-11 18:32:27 -0700

    cleanup test.cpp

--- reference_code/halide_hog/test.cpp
+++ reference_code/halide_hog/test.cpp
@@ -8,51 +8,16 @@
 #include "static_image.h"
 #include "image_io.h"
 
-timeval t1, t2;
-#define begin_timing gettimeofday(&t1, NULL); for (int i = 0; i < 10; i++) {
-#define end_timing } gettimeofday(&t2, NULL);
-
 extern "C" {
 #include "gradient.h"
 }
 
-Image<float> gradient_tester(Image<uint8_t> in) {
-    Image<float> out(in.width(), in.height(), in.channels()); //TODO: make this a float. 
-
-    // Call it once to initialize the halide runtime stuff
-    gradient(in, out);
-
-    begin_timing;
-
-    // Compute the same region of the output as blur_fast (i.e., we're
-    // still being sloppy with boundary conditions)
-    gradient(in, out);
-
-    end_timing;
-
-    return out;
-}
-
-int main(int argc, char **argv) {
-
-    std::string imgName = "../../images_640x480/carsgraz_001.image.png"; //TODO: get jpg support 
+int main(int argc, char **argv){
+    std::string imgName = "../../images_640x480/carsgraz_001.image.png"; //TODO: get jpg support
     Image<uint8_t> input = load<uint8_t>(imgName.c_str()); //only supports png and ppm
-
-    Image<float> gradient_result = gradient_tester(input);
-    double halide_time = (t2.tv_sec - t1.tv_sec) + (t2.tv_usec - t1.tv_usec) / 1000000.0f;
-
-    printf("times: %f sec\n", halide_time);
-
-#if 0
-    for (int y = 64; y < input.height() - 64; y++) {
-        for (int x = 64; x < input.width() - 64; x++) {
-            if (blurry(x, y) != speedy(x, y) || blurry(x, y) != halide(x, y))
-                printf("difference at (%d,%d): %d %d %d\n", x, y, blurry(x, y), speedy(x, y), halide(x, y));
-        }
-    }
-#endif
-
-    save(gradient_result, "./gradient.png");
-
+    //Image<uint8_t> result(input.width(), input.height(), input.channels());
+    Image<float> result(input.width(), input.height(), input.channels());
+    gradient(input, result); 
+    save(result, "./gradient.png");
     return 0;
 }
