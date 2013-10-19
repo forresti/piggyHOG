d3ba0feb346effeac819dab8ca10925c9b09b555
forresti <fiandola@gmail.com>
2013-10-18 18:16:35 -0700

    hmm interesting, the atan2 seems expensive

--- reference_code/grad_cpp/main.cpp
+++ reference_code/grad_cpp/main.cpp
@@ -28,6 +28,7 @@ inline void grad_naive(Mat img, Mat &oriImg, Mat &magImg){
             for(int channel=0; channel<3; channel++){
                 //float tmp_gradX = img.at<cv::Vec3b>(y,x+1)[channel] - img.at<cv::Vec3b>(y,x-1)[channel]; //TODO: cast pixels to float (to avoid overflow/underflow?)
                 //float tmp_gradY = img.at<cv::Vec3b>(y+1,x)[channel] - img.at<cv::Vec3b>(y-1,x)[channel];
+
                 //indexing the data directly instead of using .at -- doesn't help with perf.
                 float tmp_gradX = img.data[y*img.rows*3 + (x+1)*3 + channel] - img.data[y*img.rows*3 + (x-1)*3 + channel];
                 float tmp_gradY = img.data[(y+1)*img.rows*3 + x*3 + channel] - img.data[(y-1)*img.rows*3 + x*3 + channel];
@@ -39,6 +40,7 @@ inline void grad_naive(Mat img, Mat &oriImg, Mat &magImg){
                     max_mag = tmp_mag;
                 }
             }
+#if 0
             //this is the gradient angle
             //float ori = atan2((double)gradY, (double)gradX); //does float vs. double matter here? 
             float ori = cv::fastAtan2((double)gradY, (double)gradX);
@@ -47,6 +49,9 @@ inline void grad_naive(Mat img, Mat &oriImg, Mat &magImg){
 
             oriImg.at<float>(y, x) = ori;
             magImg.at<float>(y, x) = max_mag;
+#endif
+            oriImg.at<float>(y, x) = gradY;
+            magImg.at<float>(y, x) = gradX;
         }
     }
 }
