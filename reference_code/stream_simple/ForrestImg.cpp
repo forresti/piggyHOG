33cb28749dd80cddba8e71ef6064c401f238c774
forresti <fiandola@gmail.com>
2013-10-20 16:16:14 -0700

    my naive copy is actually SLOWER with uchar than double by a factor of 2

---
+++ reference_code/stream_simple/ForrestImg.cpp
@@ -0,0 +1,20 @@
+
+#include "helpers.h"
+#include "ForrestImg.h"
+using namespace std;
+
+ForrestImg::ForrestImg(int in_height, int in_width, int in_stride){
+    height = in_height;
+    width = in_width;
+    stride = in_stride;
+
+    //TODO: calloc?
+    //TODO: align?
+    data = (PIXEL_TYPE*)malloc(width * stride * sizeof(PIXEL_TYPE)); 
+    
+}
+
+ForrestImg::~ForrestImg(){
+    free(data);
+}
+
---
+++ reference_code/stream_simple/ForrestImg.h
@@ -0,0 +1,22 @@
+#ifndef __FORRESTIMG_H__
+#define __FORRESTIMG_H__
+#include "helpers.h"
+using namespace cv;
+
+#define PIXEL_TYPE int //you modify this 
+
+//no notion of RGB ... just a series of values (pixelType = char, short int, int, etc)
+class ForrestImg{
+  public:
+    ForrestImg(int height, int width, int stride);
+    ~ForrestImg();
+
+    PIXEL_TYPE* data;
+    int width;
+    int stride; //width+padding. note that this is row-major.
+    int height;
+};
+
+
+#endif
+
