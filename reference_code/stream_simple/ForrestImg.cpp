c0167b3d93ccba9912a6b138712d807caad122b9
forresti <fiandola@gmail.com>
2013-10-20 18:13:17 -0700

    alignment isnt helping copy speed

--- reference_code/stream_simple/ForrestImg.cpp
+++ reference_code/stream_simple/ForrestImg.cpp
@@ -1,4 +1,3 @@
-
 #include "helpers.h"
 #include "ForrestImg.h"
 using namespace std;
@@ -10,8 +9,8 @@ ForrestImg::ForrestImg(int in_height, int in_width, int in_stride){
 
     //TODO: calloc?
     //TODO: align?
-    data = (PIXEL_TYPE*)malloc(width * stride * sizeof(PIXEL_TYPE)); 
-    
+    //data = (PIXEL_TYPE*)malloc(width * stride * sizeof(PIXEL_TYPE)); 
+    data = (PIXEL_TYPE*)malloc_aligned(32, width * stride * sizeof(PIXEL_TYPE));
 }
 
 ForrestImg::~ForrestImg(){
