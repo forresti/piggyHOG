6d65d6efbfe8181e361953c760635e38f6e7d693
forresti <fiandola@gmail.com>
2013-12-01 11:24:50 +1100

    stream sse

--- reference_code/stream_simple_dbs/main.cpp
+++ reference_code/stream_simple_dbs/main.cpp
@@ -3,6 +3,8 @@
 #include <math.h>
 #include <string.h>
 #include <cassert>
+#include <xmmintrin.h>
+#include <pmmintrin.h> //for _mm_hadd_pd()
 
 #include "ForrestImg.h"
 #include "helpers.h"
@@ -21,6 +23,30 @@ void stream_simple(int height,
             outImg[y *stride + x] = img[y*stride + x];
         }
     }
+}
+
+void stream_sse(int height,
+           int width,
+           int stride,
+           pixel_t *__restrict__ img,
+           pixel_t *__restrict__ outImg){
+
+    //loadSize example: (128 bits = 16 bytes) / (4 bytes per float) = 4 bytes
+    int loadSize = sizeof(__m128) / sizeof(pixel_t);
+
+     for(int y=0; y<height; y++){
+        //for(int x=0; x<width; x++){
+        for(int x=0; x<width; x+=loadSize){
+
+            __m128 in_vec = _mm_load_ps(&img[y*stride + x]); //floating-pt version doesn't like cast to __m128i. shrug.
+            //__m128 in_vec = _mm_load_ps( (__m128*)(&img[y*stride + x]) );
+            //outImg[y *stride + x] = img[y*stride + x];
+
+
+            _mm_store_ps(&outImg[y*stride + x], in_vec);
+        }
+    }
+
 }
 
 void fill_img_with_garbage(ForrestImg& img){
@@ -49,7 +75,8 @@ printf("stride = %d \n", stride);
 
     double start_timer = read_timer();
     for(int i=0; i<n_iter; i++){
-        stream_simple(img.height, img.width, img.stride, img.data, outImg.data);
+        //stream_simple(img.height, img.width, img.stride, img.data, outImg.data);
+        stream_sse(img.height, img.width, img.stride, img.data, outImg.data);
         //memcpy(outImg.data, img.data, height * stride * sizeof(pixel_t));
     }
     double stream_time = (read_timer() - start_timer) / n_iter;
