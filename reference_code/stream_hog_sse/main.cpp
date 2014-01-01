79460065cc521a6cd3860f4f2ddd10d7ea3c4843
forresti <fiandola@gmail.com>
2014-01-01 15:12:39 -0800

    good enough... should run valgrind at some point; and diff against old code

--- reference_code/stream_hog_sse/main.cpp
+++ reference_code/stream_hog_sse/main.cpp
@@ -18,7 +18,7 @@ int main (int argc, char **argv)
     //run_tests_ori_argmax(); //unit test
     //test_computeCells_voc5_vs_streamHOG(); //unit test
 
-//    test_streamHog_oneScale_default(); //timing experiment
+    //test_streamHog_oneScale_default(); //timing experiment
     test_streamHog_pyramid(); //timing experiment
 
     return 0;
