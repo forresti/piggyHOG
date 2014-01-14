b99ab9c473dc2463c982def34a5841127e6a7b37
forresti <fiandola@gmail.com>
2014-01-13 17:15:18 -0800

    rip out fftw stuff

--- stitch_pyramid/Patchwork.cpp
+++ stitch_pyramid/Patchwork.cpp
@@ -33,14 +33,6 @@ int Patchwork::MaxRows_(0);
 int Patchwork::MaxCols_(0);
 int Patchwork::HalfCols_(0);
 
-#ifndef FFLD_HOGPYRAMID_DOUBLE
-	fftwf_plan Patchwork::Forwards_(0);
-	fftwf_plan Patchwork::Inverse_(0);
-#else
-	fftw_plan Patchwork::Forwards_(0);
-	fftw_plan Patchwork::Inverse_(0);
-#endif
-
 Patchwork::Patchwork() : padx_(0), pady_(0), interval_(0)
 {
 }
@@ -91,17 +83,6 @@ interval_(pyramid.interval())
 											  rectangles_[i].first.width());
 	}
 	
-	// Transform the planes
-	int i;
-#pragma omp parallel for private(i)
-	for (i = 0; i < nbPlanes; ++i)
-#ifndef FFLD_HOGPYRAMID_DOUBLE
-		fftwf_execute_dft_r2c(Forwards_, reinterpret_cast<float *>(planes_[i].data()->data()),
-							  reinterpret_cast<fftwf_complex *>(planes_[i].data()->data()));
-#else
-		fftw_execute_dft_r2c(Forwards_, reinterpret_cast<double *>(planes_[i].data()->data()),
-							 reinterpret_cast<fftw_complex *>(planes_[i].data()->data()));
-#endif
 }
 
 int Patchwork::padx() const
@@ -124,109 +105,6 @@ bool Patchwork::empty() const
 	return planes_.empty();
 }
 
-void Patchwork::convolve(const vector<Filter> & filters,
-						 vector<vector<HOGPyramid::Matrix> > & convolutions) const
-{
-	const int nbFilters = filters.size();
-	const int nbPlanes = planes_.size();
-	const int nbLevels = rectangles_.size();
-	
-	// Early return if the patchwork or the filters are empty
-	if (empty() || !nbFilters) {
-		convolutions.clear();
-		return;
-	}
-	
-	// Pointwise multiply the transformed filters with the patchwork's planes
-	// The performace measurements reported in the paper were done without reallocating the sums
-	// each time by making them static
-	// Even though it was faster (~10%) I removed it as it was not clean/thread safe
-	vector<vector<Patchwork::Matrix> > sums(nbFilters);
-	
-	for (int i = 0; i < nbFilters; ++i) {
-		sums[i].resize(nbPlanes);
-		
-		for (int j = 0; j < nbPlanes; ++j)
-			sums[i][j].resize(MaxRows_, HalfCols_);
-	}
-	
-	// The following assumptions are not dangerous in the sense that the program will only work
-	// slower if they do not hold
-	const int cacheSize = 32768; // Assume L1 cache of 32K
-	const int fragmentsSize = (nbPlanes + 1) * sizeof(Cell); // Assume nbPlanes < nbFilters
-	const int step = min(cacheSize / fragmentsSize,
-#ifdef _OPENMP
-						 MaxRows_ * HalfCols_ / omp_get_max_threads());
-#else
-						 MaxRows_ * HalfCols_);
-#endif
-	
-	int i;
-#pragma omp parallel for private(i)
-	for (i = 0; i <= MaxRows_ * HalfCols_ - step; i += step)
-		for (int j = 0; j < nbFilters; ++j)
-			for (int k = 0; k < nbPlanes; ++k)
-				for (int l = 0; l < step; ++l)
-					sums[j][k](i + l) =
-						filters[j].first(i + l).cwiseProduct(planes_[k](i + l)).sum();
-	
-	for (i = MaxRows_ * HalfCols_ - ((MaxRows_ * HalfCols_) % step); i < MaxRows_ * HalfCols_; ++i)
-		for (int j = 0; j < nbFilters; ++j)
-			for (int k = 0; k < nbPlanes; ++k)
-				sums[j][k](i) = filters[j].first(i).cwiseProduct(planes_[k](i)).sum();
-	
-	// Transform back the results and store them in convolutions
-	convolutions.resize(nbFilters);
-	
-	for (int i = 0; i < nbFilters; ++i)
-		convolutions[i].resize(nbLevels);
-	
-#pragma omp parallel for private(i)
-	for (i = 0; i < nbFilters * nbPlanes; ++i) {
-		const int k = i / nbPlanes; // Filter index
-		const int l = i % nbPlanes; // Plane index
-		
-		Map<HOGPyramid::Matrix, Aligned>
-			output(reinterpret_cast<HOGPyramid::Scalar *>(sums[k][l].data()), MaxRows_,
-				   HalfCols_ * 2);
-		
-#ifndef FFLD_HOGPYRAMID_DOUBLE
-		fftwf_execute_dft_c2r(Inverse_, reinterpret_cast<fftwf_complex *>(sums[k][l].data()),
-							  output.data());
-#else
-		fftw_execute_dft_c2r(Inverse_, reinterpret_cast<fftw_complex *>(sums[k][l].data()),
-							 output.data());
-#endif
-		
-		for (int j = 0; j < nbLevels; ++j) {
-			const int rows = rectangles_[j].first.height() + pady_ - filters[k].second.first + 1;
-			const int cols = rectangles_[j].first.width() + padx_ - filters[k].second.second + 1;
-			
-			if ((rows > 0) && (cols > 0) && (rectangles_[j].second == l)) {
-				const int x = rectangles_[j].first.x();
-				const int y = rectangles_[j].first.y();
-				const int width = rectangles_[j].first.width();
-				const int height = rectangles_[j].first.height();
-				
-				if ((rows <= height) && (cols <= width)) {
-					convolutions[k][j] = output.block(y, x, rows, cols);
-				}
-				else {
-					convolutions[k][j].resize(rows, cols);
-					convolutions[k][j].topLeftCorner(min(rows, height), min(cols, width)) =
-						output.block(y, x, min(rows, height), min(cols, width));
-					
-					if (rows > height)
-						convolutions[k][j].bottomRows(rows - height).fill(output(y, x));
-					
-					if (cols > width)
-						convolutions[k][j].rightCols(cols - width).fill(output(y, x));
-				}
-			}
-		}
-	}
-}
-
 bool Patchwork::Init(int maxRows, int maxCols)
 {
 	// It is an error if maxRows or maxCols are too small
@@ -237,70 +115,6 @@ bool Patchwork::Init(int maxRows, int maxCols)
 	HOGPyramid::Matrix tmp(maxRows * HOGPyramid::NbFeatures, maxCols + 2);
 	
 	int dims[2] = {maxRows, maxCols};
-	
-#ifndef FFLD_HOGPYRAMID_DOUBLE
-	// Use fftwf_import_wisdom_from_file and not fftwf_import_wisdom_from_filename as old versions
-	// of fftw seem to not include it
-	FILE * file = fopen("wisdom.fftw", "r");
-	
-	if (file) {
-		fftwf_import_wisdom_from_file(file);
-		fclose(file);
-	}
-	
-	const fftwf_plan forwards =
-		fftwf_plan_many_dft_r2c(2, dims, HOGPyramid::NbFeatures, tmp.data(), 0,
-								HOGPyramid::NbFeatures, 1,
-								reinterpret_cast<fftwf_complex *>(tmp.data()), 0,
-								HOGPyramid::NbFeatures, 1, FFTW_PATIENT);
-	
-	const fftwf_plan inverse =
-		fftwf_plan_dft_c2r_2d(dims[0], dims[1], reinterpret_cast<fftwf_complex *>(tmp.data()),
-							  tmp.data(), FFTW_PATIENT);
-	
-	file = fopen("wisdom.fftw", "w");
-	
-	if (file) {
-		fftwf_export_wisdom_to_file(file);
-		fclose(file);
-	}
-#else
-	FILE * file = fopen("wisdom.fftw", "r");
-	
-	if (file) {
-		fftw_import_wisdom_from_file(file);
-		fclose(file);
-	}
-	
-	const fftw_plan forwards =
-		fftw_plan_many_dft_r2c(2, dims, HOGPyramid::NbFeatures, tmp.data(), 0,
-							   HOGPyramid::NbFeatures, 1,
-							   reinterpret_cast<fftw_complex *>(tmp.data()), 0,
-							   HOGPyramid::NbFeatures, 1, FFTW_PATIENT);
-	
-	const fftw_plan inverse =
-		fftw_plan_dft_c2r_2d(dims[0], dims[1], reinterpret_cast<fftw_complex *>(tmp.data()),
-							 tmp.data(), FFTW_PATIENT);
-	
-	file = fopen("wisdom.fftw", "w");
-	
-	if (file) {
-		fftw_export_wisdom_to_file(file);
-		fclose(file);
-	}
-#endif
-	
-	// If successful, set MaxRows_, MaxCols_, HalfCols_, Forwards_ and Inverse_
-	if (forwards && inverse) {
-		MaxRows_ = maxRows;
-		MaxCols_ = maxCols;
-		HalfCols_ = maxCols / 2 + 1;
-		Forwards_ = forwards;
-		Inverse_ = inverse;
-		return true;
-	}
-	
-	return false;
 }
 
 int Patchwork::MaxRows()
@@ -332,15 +146,6 @@ void Patchwork::TransformFilter(const HOGPyramid::Level & filter, Filter & resul
 		for (int x = 0; x < filter.cols(); ++x)
 			plane((MaxRows_ - y) % MaxRows_, (MaxCols_ - x) % MaxCols_) = filter(y, x) /
 																		  (MaxRows_ * MaxCols_);
-	
-	// Transform that plane	
-#ifndef FFLD_HOGPYRAMID_DOUBLE
-	fftwf_execute_dft_r2c(Forwards_, reinterpret_cast<float *>(plane.data()->data()),
-						  reinterpret_cast<fftwf_complex *>(result.first.data()->data()));
-#else
-	fftw_execute_dft_r2c(Forwards_, reinterpret_cast<double *>(plane.data()->data()),
-						 reinterpret_cast<fftw_complex *>(result.first.data()->data()));
-#endif
 }
 
 namespace FFLD
